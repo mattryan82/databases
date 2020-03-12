7b99010d016ea35f462949e3b35df748d030e606
Matt Ryan <mattryanjersey@gmail.com>
2020-03-12 19:07:59 +0000

    added sql query

---
+++ sql_queries.sql
@@ -0,0 +1,5 @@
+SELECT * FROM eCommerceShop.product;
+
+insert into product (prod_description,stock_quantity,price) values ('laptop',100,999.99);
+
+insert into product (prod_description,stock_quantity,price) values ('mouse',200,19.99);
